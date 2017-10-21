73ceb5eaf7d16e16e6eb50782a75ec6bb7a0260b
jmshnds <jmshnds@gmail.com>
2017-10-20 20:09:16 -0700

    add search using user input

--- models/Listings.js
+++ models/Listings.js
@@ -6,9 +6,19 @@ var db = require('../helpers/db');
 
 // Get all listings
 exports.getAllListings = function(cb) {
-	var sql = 'SELECT listing_id, address, city, state, pincode, price FROM listing';
+	var sql = "SELECT listing_id, address, city, state, pincode, price "
+		sql += "FROM listing";
 	db.runquery(sql, cb); // Send query string and callback function
 }
 
-// Add other listing database functions
+// Get listings that are LIKE the user's search
+exports.getListingsBySearch = function(q, cb) {
+	var sql = "SELECT listing_id, address, city, state, pincode, price ";
+		sql += "FROM listing ";
+		sql += "WHERE LOWER(address) LIKE LOWER('%" + q + "%') OR ";
+		sql += "LOWER(city) LIKE LOWER('%" + q + "%') OR ";
+		sql += "LOWER(state) LIKE LOWER('%" + q + "%') OR ";
+		sql += "pincode LIKE '%" + q + "%'";
+	db.runquery(sql, cb); // Send query string and callback function
+}
 
