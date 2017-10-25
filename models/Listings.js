7a4cff63ec979121aa56d09a90ea7b0fbb0b394e
jmshnds <jmshnds@gmail.com>
2017-10-25 14:19:47 -0700

    Change pincode to zipcode in code

--- models/Listings.js
+++ models/Listings.js
@@ -6,18 +6,18 @@ var db = require('../helpers/db');
 
 // Get all listings
 exports.getAllListings = function(cb) {
-	var sql = "SELECT listing_id, address, city, state, pincode, price "
+	var sql = "SELECT listing_id, address, city, state, zipcode, price "
 		sql += "FROM listing";
 	db.runquery(sql, cb); // Send query string and callback function
 }
 
 // Get listings that are LIKE the user's search
 exports.getListingsBySearch = function(q, cb) {
-	var sql = "SELECT image, address, city, state, pincode, price ";
+	var sql = "SELECT image, address, city, state, zipcode, price ";
 		sql += "FROM listing ";
 		sql += "WHERE ";
 		sql += "LOWER(state) LIKE LOWER('%" + q + "%') OR ";
-		sql += "pincode LIKE '%" + q + "%' OR ";
+		sql += "zipcode LIKE '%" + q + "%' OR ";
 		sql += "LOWER(city) LIKE LOWER('%" + q + "%') OR ";
 		sql += "LOWER(address) LIKE LOWER('%" + q + "%')";
 	db.runquery(sql, cb); // Send query string and callback function
