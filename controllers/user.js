103bae36d8d06031d6330e2afabc302d3b7de496
jmshnds <jmshnds@gmail.com>
2017-11-07 21:33:05 -0800

    add skeleton for user controller and user model

--- controllers/user.js
+++ controllers/user.js
@@ -4,42 +4,64 @@ var router = express.Router();
 var users = require('../models/Users');
 
 /*
- * Render login page where user may login
+ * Render login page where existing user may login
+ * (User has clicked on login link)
  */
 router.get('/login', function(req, res, next) {
+	res.redirect('/home'); // temporary
 });
 
 /*
  * Logout user from this session
  */
 router.get('/logout', function(req, res, next) {
+	req.session.destroy(function(err) {
+		if(err) {
+			// logout error
+			res.redirect('/home');
+		}
+		else {
+			// logout successful
+			res.redirect('/home');
+		}
+	});
 });
 
 /*
+ * GET request to signup page
  * Render signup page where user may signup/create an account
+ * (User has clicked on the signup link)
  */
 router.get('/signup', function(req, res, next) {
+	res.redirect('/home'); // temporary
 });
 
 /*
- * Confirm user signup.
+ * POST request to signup page
  * Check that the user does not already exist in the database
  * Create a new account for the user in the database
+ * (User has submitted account information on signup page)
  */
-router.post('/signup/confirm', function(req, res, next) {
+router.post('/signup', function(req, res, next) {
+	res.redirect('/home'); // temporary
 });
 
 /*
  * GET request to user dashboard
  * Check that the user is already logged in
+ * (User clicks on dashboard link)
  */
 router.get('/dashboard', function(req, res, next) {
+	res.redirect('/home'); // temporary
 });
 
 /*
- *
+ * POST request to user dashboard
+ * Check that user credentials are correct
+ * (Assuming user is redirected to dashboard upon logging in)
  */
 router.post('/dashboard', function(req, res, next) {
+	res.redirect('/home'); // temporary
 });
 
 module.exports = router;
---
+++ models/Users.js
@@ -0,0 +1,42 @@
+/*
+ *	db module is used by models to facilitate database operations.
+ */
+var db = require('../helpers/db');
+
+/*
+ * getUsersByLogin
+ * Get users that match the provided login credentials. 
+ * q is an object that contains email and password
+ */
+exports.getUserByLogin = function(q, cb) {
+	var sql = "SELECT user_id, email, first_name, last_name, user_type ";
+		sql += "FROM user ";
+		sql += "WHERE ";
+		sql += "email = ? AND ";
+		sql += "user_password = ?";
+	db.runqueryEscaped(sql, [q.email, q.password], cb); // Send query string and callback function
+}
+
+/*
+ * getUsersById
+ * Get users that match the provided user id.
+ * q is a user id
+ */
+exports.getUserById = function(q, cb) {
+	var sql = "SELECT user_id, email, first_name, last_name, user_type ";
+		sql += "FROM user ";
+		sql += "WHERE ";
+		sql += "user_id = ?";
+	db.runqueryEscaped(sql, [q], cb); // Send query string and callback function
+}
+
+/*
+ * addNewUser
+ * q is an object that contains email and password
+ */
+exports.addNewUser = function(q, cb) {
+	var sql = "INSERT INTO user(user_type, email, user_password, i_agree) ";
+		sql += "VALUES(0, ?, ?, 1)";
+	db.runqueryEscaped(sql, [q.email, q.password], cb);
+}
+
