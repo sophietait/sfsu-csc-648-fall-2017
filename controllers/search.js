ba75891f136192548838d1ce35b070c3d0735905
Supritha Amudhu <supritha.amudhu@gmail.com>
2017-11-12 20:14:29 -0800

    Supritha - Sort by Price, Bedroom and Bathroom Count - Feature completed

--- controllers/search.js
+++ controllers/search.js
@@ -20,56 +20,28 @@ router.get('/', function(req, res, next) {
 			}
 		}
 		// pass JSON data from search controller to search view
-		res.render('search/search', { data: data, pass_search_text: req.query.search_text });
+		res.render('search/search', { data: data, pass_search_text: req.query.search_text, pass_price_count: 0, pass_bedroom_count: 0, pass_bathroom_count: 0 });
 	});
 });
 
-router.get('/sortByPriceHighToLow', function(req, res, next) {
-	listings.sortByPriceHighToLow(req.query.search_text, function(err, data) {
-		if(err) {
-			// add error handling for database errors
-			// Set data to default test listings on database error(Used for testing without database)
-			listings.getDefaultListings(function(data) {
-				res.render('search/search', { data: data });
+router.get('/sortByPriceBedBath', function(req,res,next){
+	listings.sortByPriceBedBath(req.query.search_text, req.query.sortByPrice, req.query.bedroomValue, req.query.bathroomValue, function(err,data){
+		if(err){
+			listings.getDefaultListings(function(data){
+				res.render('search/search', {data: data});
 			});
 		}
-		else {
-			// Convert image blobs to base64 encoded strings(a format that HTML can display)
-			for(var i = 0; i < data.length; i++) {
+		else{
+			for(var i=0; i<data.length;i++){
 				if(data[i].image == null){
 					continue;
 				}
 				var imgstr = new Buffer(data[i].image, 'binary').toString('base64');
-				data[i].image = 'data:image/png;base64,' + imgstr;
+				data[i].image = 'data:image/png;base64' + imgstr;
 			}
-			// pass JSON data from search controller to search view
 			res.render('partials/searchResults.ejs', {layout: true, data: data});
 		}
-	});
-});
-
-router.get('/sortByPriceLowToHigh', function(req, res, next) {
-	listings.sortByPriceLowToHigh(req.query.search_text, function(err, data) {
-		if(err) {
-			// add error handling for database errors
-			// Set data to default test listings on database error(Used for testing without database)
-			listings.getDefaultListings(function(data) {
-				res.render('search/search', { data: data });
-			});
-		}
-		else {
-			// Convert image blobs to base64 encoded strings(a format that HTML can display)
-			for(var i = 0; i < data.length; i++) {
-				if(data[i].image == null){
-					continue;
-				}
-				var imgstr = new Buffer(data[i].image, 'binary').toString('base64');
-				data[i].image = 'data:image/png;base64,' + imgstr;
-			}
-			// pass JSON data from search controller to search view
-			res.render('partials/searchResults.ejs', {layout: true, data: data});
-		}
-	});
+	})
 });
 
 module.exports = router;
