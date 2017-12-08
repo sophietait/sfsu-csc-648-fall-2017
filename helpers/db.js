97a5b2d799ba6c4d1197c7a5b2550098c11104e7
saengdueancalderaz <happygoldfish8@gmail.com>
2017-12-07 22:03:41 -0800

    switched to group database

--- helpers/db.js
+++ helpers/db.js
@@ -3,8 +3,8 @@ var mysql = require('mysql');
 // Provide database connection details
 var connection = mysql.createConnection({
 	host: '127.0.0.1',
-	user: 'root',
-	password: 'Mattem2009!',
+	user: 'fa17g05',
+	password: 'csc648fa17g05',
 	database: 'fa17g05'
 });
 
