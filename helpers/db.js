d0ab4f86e05ffadbf8ae4869a2dd79aaacd32b38
Supritha Amudhu <supritha.amudhu@gmail.com>
2017-12-19 11:41:51 -0800

    Revert "Revert "Supritha Amudhu - Add listing details in Seller messages""
    This reverts commit 1b35fafa0f4708f9b5604beda560ee08b6fa2d58.

--- helpers/db.js
+++ helpers/db.js
@@ -1,11 +1,19 @@
 var mysql = require('mysql');
 
 // Provide database connection details
+// var connection = mysql.createConnection({
+// 	host: '127.0.0.1',
+// 	user: 'fa17g05',
+// 	password: 'csc648fa17g05',
+// 	database: 'fa17g05'
+// });
+
 var connection = mysql.createConnection({
-	host: '127.0.0.1',
-	user: 'fa17g05',
-	password: 'csc648fa17g05',
-	database: 'fa17g05'
+	host: 'localhost',
+	user: 'root',
+	password: 'test',
+	database: 'fa17g05',
+	port: 3306
 });
 
 /*
